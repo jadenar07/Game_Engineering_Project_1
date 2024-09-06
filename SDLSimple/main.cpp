9eb534e69c29445834704a79d45fd26e3b7b70e7
Jaden Ritchie <ritchiejaden801@gmail.com>
2024-09-06 16:33:23 -0400

    Initial Commit

---
+++ SDLSimple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Jaden Ritchie on 9/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
